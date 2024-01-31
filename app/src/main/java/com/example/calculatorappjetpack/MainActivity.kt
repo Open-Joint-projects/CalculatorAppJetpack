1216d268e45e89f6477c8a70b13d1cb0177cb6ad
garcticman <garcticman@gmail.com>
2024-01-31 20:56:22 +0300

    Revert "commit"
    This reverts commit b63757e260f4117c1f8644a35aeb228260e5ec10.

--- app/src/main/java/com/example/calculatorappjetpack/MainActivity.kt
+++ app/src/main/java/com/example/calculatorappjetpack/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello12 $name!",
+        text = "Hello123 $name!",
         modifier = modifier
     )
 }
