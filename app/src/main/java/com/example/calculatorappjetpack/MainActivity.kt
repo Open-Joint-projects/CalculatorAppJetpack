b21103eb1daeca324f6832e04dcd47f1f2999cc4
DK <ivan.yakimishin10@gmail.com>
2024-01-31 22:04:45 +0300

    commit

--- app/src/main/java/com/example/calculatorappjetpack/MainActivity.kt
+++ app/src/main/java/com/example/calculatorappjetpack/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.calculatorappjetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculatorappjetpack.ui.theme.CalculatorAppJetpackTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,6 +31,7 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
                 }
+                CalculatorButtonsPanel()
             }
         }
     }
@@ -39,5 +50,48 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     CalculatorAppJetpackTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CalculatorButtonsPanel() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 16.dp)
+            .wrapContentHeight(Alignment.Bottom)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            CalculatorButton(text = "1")
+            CalculatorButton(text = "2")
+            CalculatorButton(text = "3")
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            CalculatorButton(text = "4")
+            CalculatorButton(text = "5")
+            CalculatorButton(text = "6")
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            CalculatorButton(text = "7")
+            CalculatorButton(text = "8")
+            CalculatorButton(text = "9")
+        }
+    }
+}
+
+@Composable
+fun CalculatorButton(text: String) {
+    Button(onClick = { /* действие по нажатию кнопки */ }) {
+        Text(text)
     }
 }
